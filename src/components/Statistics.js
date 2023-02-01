59d5235ede1992ba367beb6faddd2a5908f06643
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-01 18:39:53 +0200

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,11 +1,16 @@
-import { StatisticsList } from './StatisticsList';
-
 export function Statistics({ title, stats }) {
   return (
     title && (
       <section>
         <h2>{title}</h2>
-        <StatisticsList key={stats.id} item={stats} />
+        <ul>
+          {stats.map(({ id, label, percentage }) => (
+            <li key={id}>
+              <span>{label}</span>
+              <span>{percentage}</span>
+            </li>
+          ))}
+        </ul>
       </section>
     )
   );
